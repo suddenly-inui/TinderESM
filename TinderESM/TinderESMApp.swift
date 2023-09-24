98d47fa8ec23118b2c49e0ad23c152de02d6f122
suddenly_inui <inuiyuki0904@gmail.com>
2023-09-24 22:51:45 +0900

    Initial Commit

---
+++ TinderESM/TinderESMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderESMApp.swift
+//  TinderESM
+//
+//  Created by Yuki Inui on 2023/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderESMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
